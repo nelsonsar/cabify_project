7a4ea2dfb3511da077c14c4a170489a14086395b
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-19 13:59:57 -0200

    Add behavior #scan tests to checkout

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe Checkout do
       expect(co.total).to eq(0.00)
     end
   end
+
+  describe '#scan' do
+    context 'when the item is in catalog' do
+      it 'calls add on list of items' do
+        p = double
+        allow(ProductCatalog).to receive(:get).with('FOO').and_return(p)
+
+        co = Checkout.new
+
+        expect(co.items).to receive(:add).with(p)
+        co.scan('FOO')
+      end
+    end
+
+    context 'when the item is not in catalog' do
+      it 'outputs unavailable message' do
+        co = Checkout.new
+        expect { co.scan('FOO') }.to output("FOO is not in catalog.\n")
+          .to_stdout
+      end
+    end
+  end
 end
