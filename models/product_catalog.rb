dfd2c54d3cf3ef7b4714542a8c3afd5ee5446e8c
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-19 16:17:33 -0200

    Refactor and implement new tests on product catalog

--- models/product_catalog.rb
+++ models/product_catalog.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal
+
 require 'singleton'
 
 class ProductCatalog
@@ -6,6 +8,10 @@ class ProductCatalog
   attr_reader :items
 
   Struct.new('Product', :code, :name, :price)
+
+  HEADER = "Code         | Name                |  Price\n" \
+           '-------------------------------------------------'.freeze
+  DISPLAY_FORMAT = '%-13.13s| %-20.20s| %6.6s€'.freeze
 
   def initialize
     @items = {}
@@ -19,7 +25,19 @@ class ProductCatalog
     instance.items[code] = Struct::Product.new(code, name, price)
   end
 
+  def self.remove(code)
+    instance.items.delete(code)
+  end
+
   def self.stub_product
     Struct::Product.new('FOO', 'Cabify Foo', 10.00)
+  end
+
+  def self.display
+    puts HEADER
+    instance.items.values.each do |item|
+      price = format('%.2f', item.price)
+      puts format(DISPLAY_FORMAT, item.code, item.name, price)
+    end
   end
 end
--- spec/models/product_catalog_spec.rb
+++ spec/models/product_catalog_spec.rb
@@ -1,34 +1,58 @@
 require './models/product_catalog'
 
 RSpec.describe ProductCatalog do
+  let(:product) { ProductCatalog.stub_product }
+
   describe '.initialize' do
     it 'raises an error' do
       expect { ProductCatalog.new }.to raise_error(NoMethodError)
     end
   end
 
-  describe '#add' do
+  describe '.add' do
     it 'adds product to catalog' do
-      code = 'FOO'
-      name = 'Cabify Foo'
-      price = 10.00
-      ProductCatalog.add(code, name, price)
-      expect(ProductCatalog.instance.items['FOO']).not_to eq(nil)
+      ProductCatalog.add(product.code, product.name, product.price)
+      expect(ProductCatalog.instance.items[product.code]).not_to eq(nil)
     end
   end
 
-  describe '#get' do
+  describe '.get' do
     context 'when product is in catalog' do
       it 'returns product' do
-        code = 'FOO'
-        name = 'Cabify Foo'
-        price = 10.00
-        ProductCatalog.add(code, name, price)
-        product = ProductCatalog.get('FOO')
-        expect(product.code).to eq('FOO')
-        expect(product.name).to eq('Cabify Foo')
-        expect(product.price).to eq(10.00)
+        ProductCatalog.add(product.code, product.name, product.price)
+        product_retrived = ProductCatalog.get(product.code)
+        expect(product_retrived).to eq(product)
       end
+    end
+  end
+
+  describe '.stub_product' do
+    it 'returns a stubbed foo product' do
+      expect(product.code).to eq('FOO')
+      expect(product.name).to eq('Cabify Foo')
+      expect(product.price).to eq(10.00)
+    end
+  end
+
+  describe '.remove' do
+    it 'removes product from catalog' do
+      ProductCatalog.remove('FOO')
+      expect(ProductCatalog.instance.items['FOO']).to eq(nil)
+    end
+  end
+
+  describe '.display' do
+    it 'returns a table string with products on catalog' do
+      ProductCatalog.remove('FOO')
+      ProductCatalog.add('VOUCHER', 'Cabify Voucher', 5.00)
+      ProductCatalog.add('TSHIRT', 'Cabify T-Shirt', 20.00)
+      ProductCatalog.add('MUG', 'Cabify Coffee Mug', 7.50)
+      expected_string = "Code         | Name                |  Price\n"
+      expected_string << "-------------------------------------------------\n"
+      expected_string << "VOUCHER      | Cabify Voucher      |   5.00€\n"
+      expected_string << "TSHIRT       | Cabify T-Shirt      |  20.00€\n"
+      expected_string << "MUG          | Cabify Coffee Mug   |   7.50€\n"
+      expect { ProductCatalog.display }.to output(expected_string).to_stdout
     end
   end
 end
