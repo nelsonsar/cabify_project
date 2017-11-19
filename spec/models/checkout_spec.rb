d705b46104f0a640a50bea6e86f3b3a5d154287e
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-19 14:54:40 -0200

    Add state #scan tests to checkout

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -9,23 +9,37 @@ RSpec.describe Checkout do
   end
 
   describe '#scan' do
+    Struct.new('Product', :code, :name, :price)
+    let(:checkout) { Checkout.new }
+    let(:product) { Struct::Product.new('FOO', 'Cabify Foo', 10.00) }
+
     context 'when the item is in catalog' do
       it 'calls add on list of items' do
-        p = double
-        allow(ProductCatalog).to receive(:get).with('FOO').and_return(p)
+        allow(ProductCatalog).to receive(:get).with('FOO').and_return(product)
 
-        co = Checkout.new
+        expect(checkout.items).to receive(:add).with(product)
+        checkout.scan('FOO')
+      end
 
-        expect(co.items).to receive(:add).with(p)
-        co.scan('FOO')
+      it 'adds product to items list' do
+        allow(ProductCatalog).to receive(:get).with('FOO').and_return(product)
+
+        checkout.scan('FOO')
+
+        expect(checkout.items['FOO']).not_to eq(nil)
       end
     end
 
     context 'when the item is not in catalog' do
       it 'outputs unavailable message' do
-        co = Checkout.new
-        expect { co.scan('FOO') }.to output("FOO is not in catalog.\n")
-          .to_stdout
+        expect { checkout.scan('FOO') }
+          .to output("FOO is not in catalog.\n").to_stdout
+      end
+
+      it 'does not add product to items list' do
+        checkout.scan('FOO')
+
+        expect(checkout.items['FOO']).to eq(nil)
       end
     end
   end
