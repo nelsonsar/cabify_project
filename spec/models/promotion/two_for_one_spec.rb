5b44cc23423140eab21afe8167bbbb6cac303773
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-19 18:16:31 -0200

    Initialize two for one promotion tests

---
+++ spec/models/promotion/two_for_one_spec.rb
@@ -0,0 +1,37 @@
+require './models/promotion'
+require './models/promotion/two_for_one'
+
+RSpec.describe TwoForOne do
+  let(:product) { ProductCatalog.stub_product }
+  let(:two_for_one) { TwoForOne.new(product.code) }
+  let(:checkout) { Checkout.new }
+
+  before do
+    ProductCatalog.instance.add(product.code, product.name, product.price)
+  end
+
+  describe '#apply' do
+    context 'when promotion is not appliable' do
+      it 'does nothing' do
+        expect { two_for_one.apply(checkout) }.not_to change { checkout.total }
+      end
+    end
+
+    context 'when there is on scanned promotional product' do
+      it 'does nothing' do
+        checkout.scan(product.code)
+        expect { two_for_one.apply(checkout) }.not_to change { checkout.total }
+      end
+    end
+
+    context 'when there are more than one scanned promotional product' do
+      it 'applies two for one discount' do
+        checkout.scan(product.code)
+        checkout.scan(product.code)
+        checkout.scan(product.code)
+        expect { two_for_one.apply(checkout) }.to change { checkout.total }
+          .from(product.price * 3).to(product.price * 2)
+      end
+    end
+  end
+end
