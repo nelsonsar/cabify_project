6b7e12174aff4b1e2805e89aefafe17205e59040
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-20 13:18:24 -0200

    Move product catalog to instance variable

--- models/checkout.rb
+++ models/checkout.rb
@@ -2,20 +2,21 @@ require './models/product_catalog'
 require './models/items_list'
 
 class Checkout
-  attr_reader :items
+  attr_reader :items, :catalog
 
   def initialize
     @items = ItemsList.new
+    @catalog = ProductCatalog.instance
   end
 
   def total
     items.total
   end
 
-  def scan(product_name)
-    product = ProductCatalog.instance.get(product_name)
+  def scan(product_code)
+    product = catalog.get(product_code)
     return items.add(product) if product
-    puts "#{product_name} is not in catalog."
+    puts "#{product_code} is not in catalog."
   end
 
   def add_discount(item, quantity, total)
