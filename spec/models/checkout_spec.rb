980f241f2ba698e0ba03b440559a94a0ff5cde29
Arthur Xaud <arthurxaud@poli.ufrj.br>
2017-11-20 20:58:14 -0200

    Implement promotions tests on checkout

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -1,4 +1,6 @@
 require './models/checkout'
+require './models/promotion/bulk_per_unit_promotion'
+require './models/promotion/buy_x_for_y_promotion'
 
 RSpec.describe Checkout do
   let(:checkout) { Checkout.new }
@@ -22,6 +24,64 @@ RSpec.describe Checkout do
         expect(checkout.total).to eq(expected_total)
       end
     end
+
+    context 'when there are promotions' do
+      let(:two_for_one) { BuyXforYPromotion.new('VOUCHER', 2, 1) }
+      let(:bulk_discount) { BulkPerUnitPromotion.new('TSHIRT', 3, 19.00) }
+      let(:checkout) { Checkout.new([two_for_one, bulk_discount]) }
+
+      before do
+        catalog.add('VOUCHER', 'Cabify Voucher', 5.00)
+        catalog.add('TSHIRT', 'Cabify T-Shirt', 20.00)
+        catalog.add('MUG', 'Cabify Mug', 7.50)
+      end
+
+      context 'but they are not applicable' do
+        it 'returns the sum of quantity weighted prices' do
+          checkout.scan('VOUCHER')
+          checkout.scan('TSHIRT')
+          checkout.scan('MUG')
+
+          expect(checkout.total).to eq(32.50)
+        end
+      end
+
+      context 'and two for one promotion is applicable' do
+        it 'applies discount' do
+          checkout.scan('VOUCHER')
+          checkout.scan('TSHIRT')
+          checkout.scan('VOUCHER')
+
+          expect(checkout.total).to eq(25.00)
+        end
+      end
+
+      context 'and bulk promotion is applicable' do
+        it 'applies discount' do
+          checkout.scan('TSHIRT')
+          checkout.scan('TSHIRT')
+          checkout.scan('TSHIRT')
+          checkout.scan('VOUCHER')
+          checkout.scan('TSHIRT')
+
+          expect(checkout.total).to eq(81.00)
+        end
+      end
+
+      context 'and more than one promotion is applicable' do
+        it 'applies multiple discounts' do
+          checkout.scan('VOUCHER')
+          checkout.scan('TSHIRT')
+          checkout.scan('VOUCHER')
+          checkout.scan('VOUCHER')
+          checkout.scan('MUG')
+          checkout.scan('TSHIRT')
+          checkout.scan('TSHIRT')
+
+          expect(checkout.total).to eq(74.50)
+        end
+      end
+    end
   end
 
   describe '#scan' do
@@ -42,6 +102,7 @@ RSpec.describe Checkout do
         it 'increments items quantity' do
           checkout.scan(product.code)
           item = checkout.items[product.code]
+
           expect(item.quantity).to eq(1)
 
           checkout.scan(product.code)
